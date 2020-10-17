dc9f84b0ff814e67d74dae78012ef19c464695a6
halilraba <57082927+halilraba@users.noreply.github.com>
2020-10-17 14:53:59 -0400

    Spring Container

--- Spring-00-LooselyCoupled/src/CyApp.java
+++ Spring-00-LooselyCoupled/src/CyApp.java
@@ -1,15 +1,19 @@
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CyApp {
 
     public static void main(String[] args) {
 
         FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
 
         MentorAccount mentor = new MentorAccount(fullTime);
+        MentorAccount mentor2 = new MentorAccount(partTime);
 
         mentor.manageAccount();
+        mentor2.manageAccount();
 
     }
 
