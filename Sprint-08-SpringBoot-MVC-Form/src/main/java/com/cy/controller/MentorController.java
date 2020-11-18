3b283ca14378156b8ada3701ae9bc850110f320d
halilraba <57082927+halilraba@users.noreply.github.com>
2020-11-17 21:06:16 -0500

    MVC Form

---
+++ Sprint-08-SpringBoot-MVC-Form/src/main/java/com/cy/controller/MentorController.java
@@ -0,0 +1,36 @@
+package com.cy.controller;
+
+import com.cy.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+        model.addAttribute("mentor", new Mentor());
+
+        List<String> batchList = Arrays.asList("B1", "B2","B3", "B4");
+        model.addAttribute("batchList", batchList);
+
+
+        return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor2){
+
+        return "mentor/mentor-confirmation";
+    }
+
+
+}
